52ae37f16330467843c6e7cb470e622e077a1816
PacificCellar53 <92798379+PacificCellar53@users.noreply.github.com>
2021-11-11 09:00:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let hand = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (hand == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
